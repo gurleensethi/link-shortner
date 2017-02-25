51af1a5a8d04f6048abde8ae5fb4ac825717754d
gurleensethi <sarusethi@rocketmail.com>
2017-02-25 21:30:16 +0530

    Update LinkWithoutAccount Layout

--- app/src/main/java/app/com/thetechnocafe/linkshortner/LinkWithoutAccount/LinkWithoutAccountActivity.java
+++ app/src/main/java/app/com/thetechnocafe/linkshortner/LinkWithoutAccount/LinkWithoutAccountActivity.java
@@ -1,19 +1,35 @@
 package app.com.thetechnocafe.linkshortner.LinkWithoutAccount;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import app.com.thetechnocafe.linkshortner.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LinkWithoutAccountActivity extends AppCompatActivity implements LinkWithoutAccountContract.View {
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
     private LinkWithoutAccountContract.Presenter mPresenter;
+
+    //Return the Intent required to launch this Activity
+    public static Intent getIntent(Context context) {
+        Intent intent = new Intent(context, LinkWithoutAccountActivity.class);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_without_account);
+
+        ButterKnife.bind(this);
 
         mPresenter = new LinkWithoutAccountPresenter();
         mPresenter.attachView(this);
@@ -26,6 +42,40 @@ public class LinkWithoutAccountActivity extends AppCompatActivity implements Lin
 
     @Override
     public void initViews() {
+        //Configure Toolbar
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_left_white);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPresenter.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPresenter.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detachView();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/app/com/thetechnocafe/linkshortner/SignIn/SignInActivity.java
+++ app/src/main/java/app/com/thetechnocafe/linkshortner/SignIn/SignInActivity.java
@@ -1,12 +1,19 @@
 package app.com.thetechnocafe.linkshortner.SignIn;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import app.com.thetechnocafe.linkshortner.LinkWithoutAccount.LinkWithoutAccountActivity;
 import app.com.thetechnocafe.linkshortner.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignInActivity extends AppCompatActivity implements SignInContract.View {
+
+    @BindView(R.id.shorten_link_without_account_text_view)
+    TextView mShortenLinkWithoutAccountTextView;
 
     private SignInContract.Presenter mPresenter;
 
@@ -14,6 +21,8 @@ public class SignInActivity extends AppCompatActivity implements SignInContract.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        ButterKnife.bind(this);
 
         mPresenter = new SignInPresenter();
         mPresenter.attachView(this);
@@ -26,6 +35,27 @@ public class SignInActivity extends AppCompatActivity implements SignInContract.
 
     @Override
     public void initViews() {
+        mShortenLinkWithoutAccountTextView.setOnClickListener(view -> {
+            //Launch LinkWithoutAccountActivity
+            startActivity(LinkWithoutAccountActivity.getIntent(getApplicationContext()));
+        });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPresenter.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPresenter.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detachView();
     }
 }
